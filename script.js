c67186ef7de5ec52b09bab37fab8796ae93f735b
GuilhermeMonni <135198986+GuilhermeMonni@users.noreply.github.com>
2023-10-24 11:24:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+function clickRick(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoRick.jpg"
+    txtCont.innerHTML = "Richard Sanchez da Dimensão C-137, comumente referido como Rick C-137, é o protagonista principal da franquia Rick e Morty. Ele é um cientista megagênio cujo alcoolismo e comportamento imprudente são fontes de preocupação para a sua família. Após o início da série, as origens misteriosas e a saúde mental de Rick eram grandes fontes de especulação. Com o passar do tempo, mais foi revelado sobre seu personagem; sua história explica muito de seu comportamento. Ao mesmo tempo, Rick tem cometido continuamente atos mais hediondos, certificando-o como o anti-herói da série, beirando o antagonista."
+    tituloCont.innerHTML = "Rick Sanchez"
+}
+
+function clickMorty(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoMorty.jpg"
+    txtCont.innerHTML = "Mortimer Chauncey Smith Sr. do Universo Primordial/Dimensão Cronenberged é o protagonista central titular de Rick e Morty. Ele também é um dos cinco personagens principais. Ele é neto de Rick Sanchez e muitas vezes é forçado a acompanhar suas várias desventuras. Morty estuda na Harry Herpson High School junto com sua irmã, Summer. No início da série, Morty mostrou ser um adolescente incrivelmente ansioso e submisso. No entanto, depois de passar por muitas supostas aventuras com Rick, Morty aparentemente se tornou mais confiante e independente de Rick, ao invés de um mero companheiro."
+    tituloCont.innerHTML = "Morty Smith"
+}
+
+function clickSummer(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoSummer.jpg"
+    txtCont.innerHTML = "Summer Smith da Dimensão C-131 é o tritagonista de Rick e Morty, e um dos cinco personagens principais. Ela é filha de Jerry Smith e Beth Smith / Space Beth, irmã mais velha de Morty Smith, e mãe de Naruto Smith. Atualmente ela atua como irmã mais velha de Morty e neta Rick, respectivamente, da Dimensão Cronenberged e da Dimensão C-137. Ela é o único outro membro conhecido da Família Smith que está ciente desse fato. Embora em &#34Solaricks&#34 toda a família descubra que Rick e Morty são de outro universo. À medida que a série avançava, Summer tornou-se cada vez mais envolvida nas aventuras de Rick e Morty. Ela e Morty frequentemente brigam pelo afeto de Rick, mas também mantêm um vínculo único decorrente da revelação de que Morty não é seu irmão original."
+    tituloCont.innerHTML = "Summer Smith"
+}
+
+function clickBeth(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoBeth.jpg"
+    txtCont.innerHTML = "Bethany Smith da Dimensão C-131 é uma das cinco personagens principais de Rick e Morty. Ela é filha de Rick e Diane Sanchez, esposa de Jerry Smith, e mãe de Summer Smith e do falecido Morty Smith. Atualmente ela atua como mãe Morty Smith e filha de Rick Sanchez da dimensão Cronenberged e da dimensão C-137. Ela também trabalha como veterinária. Caracterizada por um complexo de superioridade e problemas de abandono, Beth gradualmente começou a lutar com o marido sobre as contribuições dele para o casamento, em parte devido à sua posição de nível inferior e ao emprego subsequente, impulsionado ainda mais pelo fato de seu pai influenciar seus sentimentos de superioridade. Os dois eventualmente se divorciaram, antes de se reunirem no final da 3ª temporada."
+    tituloCont.innerHTML = "Beth Smith"
+}
+
+function clickJerry(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoJerry.jpg"
+    txtCont.innerHTML = "Jerry Smith da Dimensão 5126 é um dos cinco personagens principais. Jerry é marido de Beth Smith, pai de Summer Smith e Morty Smith, e genro de Rick Sanchez. Os primeiros seis episódios da 1ª temporada apresentaram Jerry Smith Prime, enquanto os episódios subsequentes apresentaram um Jerry que nasceu e habitou a dimensão Substituição até o episódio 2 da 2ª temporada, onde trocou de lugar com este Jerry no Jerryboree. Jerry é frequentemente considerado um perdedor. Ele constantemente tenta encontrar uma carreira sem sucesso, deixando sua esposa Beth como o ganha-pão da família. No entanto, ele é capaz de heroísmo em tempos de crise e salvou a vida de sua família em algumas ocasiões. O casamento de Jerry com Beth é muitas vezes difícil e instável, resultando no divórcio dos dois na estreia da 3ª temporada. Jerry passaria a maior parte da temporada morando sozinho e deprimido em um pequeno apartamento, até que ele e Beth se reconciliassem no final da 3ª temporada, embora atualmente não se saiba se a Beth com quem ele se casou novamente é um clone ou a Beth original."
+    tituloCont.innerHTML = "Jerry Smith"
+}
+
+function clickSquanchy(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoSquanchy.jpg"
+    txtCont.innerHTML = "Squanchy é uma criatura antropomórfica parecida com um gato que foi convidada para a festa de Rick e Summer em &#34Ricksy Business&#34. Ele é um personagem recorrente em Rick e Morty, e muito amigo de Rick, compartilhando seu amor pelo álcool. Morty e Jessica conhecem Squanchy pela primeira vez praticando asfixia auto-erótica e masturbação na garagem de Morty. Isso marca a primeira entre muitas coisas que ele chama de &#34esmagamento&#34."
+    tituloCont.innerHTML = "Squanchy"
+}
+
+function clickGoldenford(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoGoldenford.jpg"
+    txtCont.innerHTML = "Goldenfold é o professor de matemática de Morty na Harry Herpson High School. Ele é dublado por Brandon Johnson. Não se sabe muito sobre a história do Sr. Goldenfold além do fato de ele ser divorciado, conforme revelado em &#34Something Ricked This Way Comes&#34. Ele tem uma aparente atração pela personagem de televisão Sra. Pancakes, com quem ele tem sonhos românticos, como visto em &#34Lawnmower Dog&#34. Ele foi visto brevemente em &#34Rick Potion #9&#34 quando estava sob o efeito da poção do amor que Rick deu a Morty. Neste mesmo episódio, o Sr. Goldenfold, junto com o resto das pessoas no mundo (exceto Rick, Morty e a família Smith), foi infectado com uma doença que fez com que ele se transformasse em um Cronenberg."
+    tituloCont.innerHTML = "Mr. Goldenford"
+}
+
+function clickMortyjr(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoMortyjr.jpg"
+    txtCont.innerHTML = "Morty Jr, possivelmente com intenção de ser expandido como Mortimer &#34Morty&#34 Smith Jr, é filho de Morty Smith. Ele foi concebido por Morty e um robô sexual Gazorpiano chamado Gwendolyn no episódio &#34Raising Gazorpazorp&#34. Morty Jr. cresceu de uma criança para um adulto em uma taxa hiper-aumentada, permitindo-lhe superar rapidamente seu pai."
+    tituloCont.innerHTML = "Morty Jr."
+}
+
+function clickArthricia(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoArthricia.png"
+    txtCont.innerHTML = "Arthricia é uma magdaliana que vive em um planeta distante que apareceu no episódio &#34Olha quem está purgando agora&#34. Ela é uma garotinha Amish graciosa, mas tortuosa, e é um humanóide parecido com um gato. Ela não concorda com os expurgos anuais, nos quais as pessoas de seu planeta agem de acordo com seu desejo de violência reprimido (resultando em todos tentando matar uns aos outros, a menos que se escondam) e lutam todos os anos para sobreviver durante eles."
+    tituloCont.innerHTML = "Arthricia"
+}
+
+function clickBirdperson(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoBirdperson.jpg"
+    txtCont.innerHTML = "Birdperson, brevemente também conhecido como Phoenixperson, é o melhor amigo de Rick Sanchez. Ele conhece Rick há muito tempo e aparentemente conhece Morty desde que ele era bebê, como revelado no episódio &#34Get Schwifty&#34. No final da 4ª temporada, sua forma ciborgue foi desmantelada e ficou em posse de Rick antes de ele ser finalmente restaurado à sua forma original em &#34Rickternal Friendshine of the Spotless Mort&#34. De acordo com Tammy Guterman, o sobrenome de Birdperson é um apito que parece começar em um registro G6# que desliza para cima e imediatamente para baixo na escala, semelhante ao apito de um pássaro; isso é mencionado brevemente no episódio &#34Os saqueadores de casamento&#34."
+    tituloCont.innerHTML = "Pessoa pássaro"
+}
+
+function clickAlbertEinstein(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoAlbert.jpg"
+    txtCont.innerHTML = "Albert Einstein foi um cientista que inventou a teoria da relatividade. Ele apareceu no episódio &#34A Rickle in Time&#34 e é baseado no cientista da vida real de mesmo nome. O Ser da Quarta Dimensão, após ter sido espancado até ficar inconsciente por Rick, fazendo com que ele o perdesse e não conseguisse prendê-lo por seus crimes contra o espaço-tempo, passou pelo tempo com seu parceiro procurando por ele. Eles finalmente encontraram Albert Einstein em seu laboratório e começaram a espancá-lo até deixá-lo sem sentido, acreditando que ele era Rick e disseram-lhe para não mexer com o tempo. Depois que eles saíram, Albert rapidamente se levantou e disse com raiva para si mesmo que daquele ponto em diante ele iria mexer com o tempo. Esta parece ser a história de Einstein no universo que o levou a publicar teorias sobre o tempo, como a teoria geral da relatividade de Einstein."
+    tituloCont.innerHTML = "Albert Einstein"
+}
+
+function clickPrincipe(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoPrincipe.jpg"
+    txtCont.innerHTML = "O Príncipe Nebulon era o líder dos Zigerions em Rick e Morty. Ele aparece como o principal antagonista em &#34M. Night Shaym-Aliens!&#34. Ele tem um rancor de longa data contra Rick Sanchez e está empenhado em roubar dele a receita de Matéria Escura Concentrada. Ele aparentemente está preocupado com a descoloração das abas do traseiro."
+    tituloCont.innerHTML = "Principe Nebulon"
+}
+
+function clickProfessorbc(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoBundacagada.png"
+    txtCont.innerHTML = "Bunda Cagada, também conhecido como Professor Bunda Cagada, é um amigo de longa data da Família Smith e um personagem recorrente da série. Ele é mostrado principalmente quebrando a quarta parede durante o show. Bunda Cagada é um grande amigo da Família Smith, de outra dimensão (onde ele é um superstar do planeta) que se refugiou nos Smiths para poder viver uma vida normal, ele é descrito como um amigo de longa data da família. Rick claramente o considera uma pessoa confiável há muito tempo, embora não se saiba há quanto tempo eles se conhecem. Ele fez sua primeira aparição no episódio &#34Total Rickall&#34 da 2ª temporada. Ele é dublado por Justin Roiland."
+    tituloCont.innerHTML = "Sr. Bunda Cagada"
+}
+
+function clickAfroRick(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoAfrorick.jpg"
+    txtCont.innerHTML = "Ele apareceu pela primeira vez em &#34The Rickshank Rickdemption&#34, na Cidadela conversando com outros Ricks enquanto Morty e Summer eram escoltados por eles até o Conselho dos Ricks. Ele estava conversando com Alien Rick e um Rick não identificado.Ele é visto mais tarde em &#34The Ricklantis Mixup&#34, aparecendo pela primeira vez nos bastidores durante o debate presidencial, depois que Juggling Rick é visto na TV. Ele está em uma discussão acalorada com Cowboy Rick. Mais tarde, ele é visto no meio da multidão de Ricks no momento da tentativa de assassinato do Presidente Morty e fica muito chateado após o tiroteio."
+    tituloCont.innerHTML = "Rick Afro"
+}
+
+function clickRickBobo(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoRickbobo.png"
+    txtCont.innerHTML = "Rick Sanchez da Dimension J19 Zeta 7, comumente conhecido como Rick bobo, é o menos respeitado de todos os Ricks. Ele apareceu pela primeira vez no episódio &#34Close Rick-counters of the Rick Kind&#34. Outros Ricks afirmam que a dimensão doméstica de Rick bobo tem como tema pessoas comendo suas próprias fezes; no entanto, Justin Roiland confirmou que Rick bobo não faz realmente a ação mencionada acima, o que significa que os outros Ricks estão apenas intimidando-o. Rick bobo era um membro da milícia da Cidadela."
+    tituloCont.innerHTML = "Rick Bobo"
+}
+
+function clickPeidar(){
+    let imgCont = document.getElementById("imgCont")
+    let txtCont = document.getElementById("txtCont")
+    let tituloCont = document.getElementById("tituloCont")
+
+    imgCont.src = "imagens/fundoPeidar.png"
+    txtCont.innerHTML = "Peido é um ser gasoso que apareceu no episódio &#34Mortynight Run&#34 da 2ª temporada. Embora tecnicamente sem nome, assumiu o nome de &#34Peido&#34 por conveniência depois que Rick o insultou. Ele se comunica por telepatia. Ele fez amizade com Morty, que queria salvar sua vida de Krombopulos Michael, o assassino Gromflomita. Seu comportamento amigável disfarçou o desejo de sua espécie de limpar o universo de quaisquer formas de vida baseadas em carbono, que eles consideram uma ameaça a toda a &#34vida superior&#34 e uma &#34doença&#34."
+    tituloCont.innerHTML = "Peido"
+}
